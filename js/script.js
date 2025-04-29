53a59aae99eca3f8d8b2e413f634b90fc63716e2
bagoesihsant <bagoesihsant@gmail.com>
2025-04-29 15:35:58 +0700

    Modified script.js, add method for animation, and add temporary function for alert notification(s).

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 console.log("File connected");
 
 // Get HTML Elements
+let triviaSection = document.getElementById("trivia-section");
+let alertDomain = document.getElementById("alert-domain");
 
 // Form Elements
 let formCalculator = document.getElementById("form-calc-suhu");
@@ -15,8 +17,6 @@ let formButtonGroup = document.getElementById("btn-group");
 
 let textAreaMethod = document.getElementById("txt-calc-method");
 let textAreaGroup = document.getElementById("calc-method");
-
-let triviaSection = document.getElementById("trivia-section");
 
 // Form Buttons
 let btnConversion = document.getElementById("convert");
@@ -45,12 +45,67 @@ function resetForm() {
     resultTempDeg = undefined;
 }
 
+function hasClass(elem, className) {
+    return elem.className.split(" ").indexOf(className) > -1;
+}
+
 function calculateCelsiusToFahrenheit(tempDeg) {
     return (tempDeg * 9/5) + 32;
 }
 
 function calculateFahrenheitToCelsius(tempDeg) {
     return (tempDeg - 32) * 5/9;
+}
+
+function closeNotif(element) {
+    element.classList.toggle("closed");
+}
+
+function createError(errorMessage) {
+
+    // Alert Notifications Body
+    let alertBody = document.createElement("div");
+    alertBody.className = "alert-notif";
+
+    // Alert Header
+    let alertHeader = document.createElement("div");
+    alertHeader.className = "alert-header";
+
+    // Error Symbol
+    let alertErrorSymbol = document.createElement("i");
+    alertErrorSymbol.className = "error";
+    alertErrorSymbol.textContent = String.fromCharCode(9888);
+
+    // Alert Title
+    let alertTitle = document.createElement("p");
+    alertTitle.className = "alert-title";
+    alertTitle.textContent = "Warning";
+    
+    // Close Symbol
+    let alertCloseSymbol = document.createElement("i");
+    alertCloseSymbol.className = "alert-close";
+    alertCloseSymbol.textContent = String.fromCharCode(88);
+
+    // Alert Body
+    let alertNotifBody = document.createElement("div");
+    alertNotifBody.className = "alert-body";
+
+    // Alert Text
+    let alertNotifyText = document.createElement("p");
+    alertNotifyText.className = "alert-text";
+    alertNotifyText.textContent = errorMessage;
+
+    // Assembling
+    alertNotifBody.appendChild(alertNotifyText);
+
+    alertHeader.appendChild(alertErrorSymbol);
+    alertHeader.appendChild(alertTitle);
+    alertHeader.appendChild(alertCloseSymbol);
+
+    alertBody.appendChild(alertHeader);
+    alertBody.appendChild(alertNotifBody);
+
+    return alertBody;
 }
 
 // Main Function (temperature conversion)
@@ -66,19 +121,19 @@ btnConversion.onclick = (event) => {
         tempDeg = inputCelsius.value;
 
         if (tempDeg === "") {
-            console.log("Input is empty.");
+            alertDomain.appendChild(createError("Input kosong."));
             resetForm();
             return;
         }
 
         if (!degreeRegex.test(tempDeg)) {
-            console.log("Input contain illegal character(s), plase input positive/negative number only.");
+            alertDomain.appendChild(createError("Input mengandung karakter illegal, harap masukkan hanya angka positif atau negatif."));
             resetForm();
             return;
         }
 
         if (isNaN(parseFloat(tempDeg))) {
-            console.log("Input is not a number.");
+            alertDomain.appendChild(createError("Input bukan angka"));
             resetForm();
             return;
         }
@@ -93,22 +148,23 @@ btnConversion.onclick = (event) => {
         tempDeg = inputFahrenheit.value;
 
         if (tempDeg === "") {
-            console.log("Input is empty.");
+            alertDomain.appendChild(createError("Input kosong."));
             resetForm();
             return;
         }
 
         if (!degreeRegex.test(tempDeg)) {
-            console.log("Input contain illegal character(s), plase input positive/negative number only.");
+            alertDomain.appendChild(createError("Input mengandung karakter illegal, harap masukkan hanya angka positif atau negatif."));
             resetForm();
             return;
         }
 
         if (isNaN(parseFloat(tempDeg))) {
-            console.log("Input is not a number.");
+            alertDomain.appendChild(createError("Input bukan angka"));
             resetForm();
             return;
         }
+
 
         resultTempDeg = calculateFahrenheitToCelsius(parseFloat(tempDeg).toFixed(2));
 
@@ -177,6 +233,22 @@ for (let triviaIndex = 0; triviaIndex < triviaTitle.length; triviaIndex++) {
     triviaTitle[triviaIndex].onclick = (event) => {
         bodysDetail[0].classList.toggle("open");
         arrowsDetail[0].classList.toggle("rotate");
-    }
+    };
 
 }
+
+document.addEventListener('click', (event) => {
+
+    if (hasClass(event.target, 'alert-close')) {
+
+        let alertNotif = event.target.parentElement.parentElement;
+
+        event.target.onclick = () => {
+            alertNotif.classList.toggle("closed");
+        };
+
+        setTimeout(() => { alertNotif.classList.toggle("closed"); }, 2000);
+
+    };
+
+});
